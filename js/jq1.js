371583c24588cf8368204955a487e8dfa383198a
IvyHole <41639410+IvyHole@users.noreply.github.com>
2018-08-05 14:54:07 +0800

    addons

---
+++ js/jq1.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+"use strict"
+$(function(){
+	$("#icmd").click(function(){
+		$("#dcmd").toggle();
+	});
+});
+$(function(){
+	$("#dclose").click(function(){
+		$("#close").slideToggle("slow");
+	});
+	$('#cllose').on('click', function() {
+        if(confirm("Are you sure to close this page？")) {
+            window.opener = null;
+            window.open('', '_self');
+            window.close();
+        } else {}
+    
+    });
+	$("#reload").click(function(){
+		location.reload();
+	});
+});
