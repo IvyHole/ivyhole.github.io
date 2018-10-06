1b7994b8f4abd80c223985f323b675eee8dee8bb
IvyHole <41639410+IvyHole@users.noreply.github.com>
2018-10-06 19:12:11 +0800

    Delete

--- 2019/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCQCHcBmJFJdDLFMQJDaYLQ9fod6s1u4VA');
-define('GJ_CODE', 'hk');
-define('SITE_NAME', 'youtube');
-define('TITLENAME', 'youtube');
-define('EN2DEKEY', '123456');
-define('EMAIL', '123@gmail.com');
-?>
